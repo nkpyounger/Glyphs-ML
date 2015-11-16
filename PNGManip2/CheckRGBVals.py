6281c9a306ffa796793fad1857384a6b8f85f950
nkpyounger <nkpyounger@gmail.com>
2015-11-16 14:11:48 -0500

    Further unpacking of glyph PNGs
    Code to check whether all values of an RGB triple are identical, and
    code to simplify array of RGBA pixel values to single byte per pixel

---
+++ PNGManip2/CheckRGBVals.py
@@ -0,0 +1,59 @@
+from PIL import Image
+import numpy as np
+
+if __name__ == '__main__':
+    im = Image.open("Wb01-Vorwort-A-1501.png") #Can be many different formats.
+    pix = im.load()
+    print im.size #Get the width and hight of the image for iterating over
+    print pix[19,17] #Get the RGBA Value of the a pixel of an image
+    #pix[x,y] = value # Set the RGBA Value of the image (tuple)
+    print type(pix[55,25])
+    
+    
+    (xmax, ymax) = im.size
+    print xmax
+    print ymax
+    
+#    pxarray = np.zeros((xmax, ymax))
+    pxarray = []
+    
+    for i in range(xmax):
+        for j in range(ymax):
+            pxarray.append(list(pix[i,j]))
+            
+    print pxarray
+    print len(pxarray)
+    
+    pxmatrix = np.reshape(pxarray, (xmax, ymax, 4))
+    # see later checking - this works
+    
+    print pxmatrix
+    
+    print pxmatrix.shape
+    print pxarray[0]
+    
+    print "pixel check"
+    print pxarray[467]
+    print pix[19,17]
+    print pxmatrix[19][17]
+    
+    for i in range(xmax):
+        for j in range(ymax):
+            if (np.array_equal(list(pix[i,j]),pxmatrix[i][j])):
+                #print "TRUE"
+                continue
+            else:
+                 print "ERROR"   
+                 print list(pix[i,j])
+                 print pxmatrix[i][j]
+                 
+    for i in range(xmax):
+        for j in range(ymax):
+            if (pxmatrix[i][j][0] == pxmatrix[i][j][1] and pxmatrix[i][j][0] == pxmatrix[i][j][2]):
+                continue
+            else:
+                print "error"
+                print pxmatrix[i][j]
+                print "at {}, {}".format(i,j)
+                
+#ALL RGB Vals are same
